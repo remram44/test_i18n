6ba05fab500127bbf52519cf4da7c33087d23eab
Remi Rampin <remirampin@gmail.com>
2013-10-13 12:41:57 -0400

    Fixes setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from __future__ import unicode_literals
-
 import os
 
 from setuptools import setup
 from setuptools.command.install_lib import install_lib as _install_lib
 from distutils.cmd import Command
 from distutils.command.build import build as _build
+from distutils.command.install_data import install_data as _install_data
 
 import msgfmt
 
@@ -56,6 +55,17 @@ class install_lib(_install_lib):
         _install_lib.run(self)
 
 
+class install_data(_install_data):
+    def run(self):
+        print "installing translation files"
+        for lang in os.listdir('build/locale'):
+            lang_dir = os.path.join('share', 'locale', lang, 'LC_MESSAGES')
+            lang_file = os.path.join('build', 'locale', lang, 'LC_MESSAGES', '%s.mo' % NAME)
+            print "registering %s" % lang_file
+            self.data_files.append((lang_dir, [lang_file]))
+        _install_data.run(self)
+
+
 setup(name=NAME,
       version='0.1',
       packages=['test_i18n'],
@@ -67,7 +77,9 @@ setup(name=NAME,
         'test_i18n': ['l10n/*.mo'],
       },
       cmdclass={
-        'build': build, 'install_lib': install_lib,
+        'build': build,
+        'install_lib': install_lib,
+        'install_data': install_data,
         'compile_translations': compile_translations},
       keywords=['python', 'i18n', 'gettext'],
       classifiers=[
