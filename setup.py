93ecbc3a61b593a1ec51ca8a8ed7ea8943de37ca
Remi Rampin <remirampin@gmail.com>
2013-10-18 17:57:01 -0400

    Adds entry_point declaration

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ add_translations(
 setup(name='test_i18n',
       version='0.1',
       packages=['test_i18n'],
+      entry_points={
+          'console_scripts': ['say_hello = test_i18n.main:main']},
       description="This is a test package for gettext",
       author="Remi Rampin",
       author_email='remirampin@gmail.com',
