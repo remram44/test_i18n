278fc2cd1374086af2d54afec35d64a737123f40
Remi Rampin <remirampin@gmail.com>
2013-10-21 18:57:38 -0400

    Use glob again for *.mo files

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-import os
 from setuptools import setup
-
-
-translation_files = []
-def add_translations(realpath, path):
-    for f in os.listdir(realpath):
-        rf = os.path.join(realpath, f)
-        pf = os.path.join(path, f)
-        if os.path.isdir(rf):
-            add_translations(rf, pf)
-        elif os.path.isfile(rf):
-            translation_files.append(pf)
-add_translations(
-        os.path.join(os.path.dirname(__file__), 'test_i18n', 'l10n'),
-        'l10n')
 
 
 setup(name='test_i18n',
@@ -26,7 +11,7 @@ setup(name='test_i18n',
       author_email='remirampin@gmail.com',
       license='Public Domain',
       package_data={
-        'test_i18n': translation_files,
+        'test_i18n': ['l10n/*/LC_MESSAGES/*.mo'],
       },
       keywords=['python', 'i18n', 'gettext'],
       classifiers=[
