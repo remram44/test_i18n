9ddf5a2a9a7df6f183e6877e2a85b841c32cd0a6
Remi Rampin <remirampin@gmail.com>
2013-10-21 18:25:55 -0400

    New setup.py (from deluge)
    Conflicts:
    	setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 import os
 from setuptools import setup
-from setuptools.command.install_lib import install_lib as _install_lib
 from distutils.cmd import Command
 from distutils.command.build import build as _build
-from distutils.command.install_data import install_data as _install_data
 
 import msgfmt
 
@@ -13,23 +11,26 @@ NAME = 'test_i18n'
 
 class compile_translations(Command):
     description = "compile .po files into .mo files"
-    user_options = []
+    user_options = [
+        ('build-lib', None, "lib build folder")]
 
     def initialize_options(self):
-        pass
- 
+        self.build_lib = None
+
     def finalize_options(self):
-        pass
- 
+        self.set_undefined_options('build', ('build_lib', 'build_lib'))
+
     def run(self):
-        po_dir = os.path.join(os.path.dirname(os.curdir), 'po')
+        po_dir = os.path.join(os.path.dirname(__file__), 'po')
+        print "Compiling po files..."
         for path, names, filenames in os.walk(po_dir):
             for f in filenames:
                 if f.endswith('.po'):
                     lang = f[:-3]
                     src = os.path.join(path, f)
                     dest_path = os.path.join(
-                            'build', 'locale', lang, 'LC_MESSAGES')
+                            self.build_lib,
+                            NAME, 'l10n', lang, 'LC_MESSAGES')
                     dest = os.path.join(dest_path, '%s.mo' % NAME)
                     if not os.path.exists(dest_path):
                         os.makedirs(dest_path)
@@ -48,23 +49,6 @@ class build(_build):
     sub_commands = [("compile_translations", None)] + _build.sub_commands
 
 
-class install_lib(_install_lib):
-    def run(self):
-        self.run_command("compile_translations")
-        _install_lib.run(self)
-
-
-class install_data(_install_data):
-    def run(self):
-        print "installing translation files"
-        for lang in os.listdir('build/locale'):
-            lang_dir = os.path.join('share', 'locale', lang, 'LC_MESSAGES')
-            lang_file = os.path.join('build', 'locale', lang, 'LC_MESSAGES', '%s.mo' % NAME)
-            print "registering %s" % lang_file
-            self.data_files.append((lang_dir, [lang_file]))
-        _install_data.run(self)
-
-
 setup(name=NAME,
       version='0.1',
       packages=['test_i18n'],
@@ -75,12 +59,10 @@ setup(name=NAME,
       author_email='remirampin@gmail.com',
       license='Public Domain',
       package_data={
-        'test_i18n': ['l10n/*.mo'],
+        'test_i18n': ['l10n/*/LC_MESSAGES/*.mo'],
       },
       cmdclass={
         'build': build,
-        'install_lib': install_lib,
-        'install_data': install_data,
         'compile_translations': compile_translations},
       keywords=['python', 'i18n', 'gettext'],
       classifiers=[
